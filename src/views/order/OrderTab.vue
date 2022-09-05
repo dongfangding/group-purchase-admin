33783221b8b8d9412ac7a0da883fcede8ada57a0
dongfang.ding <>
2022-09-05 21:37:58 +0800

    参团状态修改

--- src/views/order/OrderTab.vue
+++ src/views/order/OrderTab.vue
@@ -2,9 +2,9 @@
 	<div>
 		<el-tabs v-model="activeName" class="demo-tabs">
 			<el-tab-pane label="全部" name="all"><OrderIndex joinStatus="null" /></el-tab-pane>
-			<el-tab-pane label="待支付" name="wait_pay"><OrderIndex joinStatus="0" /></el-tab-pane>
-			<el-tab-pane label="已完成" name="complete"><OrderIndex joinStatus="1" /></el-tab-pane>
-			<el-tab-pane label="已退款" name="refund"><OrderIndex joinStatus="2" /></el-tab-pane>
+			<el-tab-pane label="待支付" name="wait_pay"><OrderIndex joinStatus="WAIT_PAY" /></el-tab-pane>
+			<el-tab-pane label="已完成" name="complete"><OrderIndex joinStatus="COMPLETE" /></el-tab-pane>
+			<el-tab-pane label="已退款" name="refund"><OrderIndex joinStatus="REFUND" /></el-tab-pane>
 		</el-tabs>
 	</div>
 </template>

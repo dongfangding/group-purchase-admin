8bb9af4cde7118f708975e4da59634a9b8e85342
dongfang.ding <>
2022-09-05 21:47:25 +0800

    修复默认状态错误

--- src/views/order/OrderTab.vue
+++ src/views/order/OrderTab.vue
@@ -1,10 +1,11 @@
 <template>
 	<div>
 		<el-tabs v-model="activeName" class="demo-tabs">
-			<el-tab-pane label="全部" name="all"><OrderIndex joinStatus="null" /></el-tab-pane>
+			<el-tab-pane label="全部" name="all"><OrderIndex joinStatus="" /></el-tab-pane>
 			<el-tab-pane label="待支付" name="wait_pay"><OrderIndex joinStatus="WAIT_PAY" /></el-tab-pane>
 			<el-tab-pane label="已完成" name="complete"><OrderIndex joinStatus="COMPLETE" /></el-tab-pane>
 			<el-tab-pane label="已退款" name="refund"><OrderIndex joinStatus="REFUND" /></el-tab-pane>
+			<el-tab-pane label="已关闭" name="closed"><OrderIndex joinStatus="CLOSED" /></el-tab-pane>
 		</el-tabs>
 	</div>
 </template>

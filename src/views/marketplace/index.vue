82b4b296b983cac84769c5a0fbefe30b8335c04c
dongfang.ding <>
2022-09-13 20:59:38 +0800

    团购市场接入真实图片属性

--- src/views/marketplace/index.vue
+++ src/views/marketplace/index.vue
@@ -27,15 +27,6 @@ const ruleForm = reactive({
 const rules = reactive({
 	goodNum: [{ required: true, message: "请选择团购数量", trigger: "blur" }]
 });
-const srcList = [
-	"https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg",
-	"https://fuss10.elemecdn.com/1/34/19aa98b1fcb2781c4fba33d850549jpeg.jpeg",
-	"https://fuss10.elemecdn.com/0/6f/e35ff375812e6b0020b6b4e8f9583jpeg.jpeg",
-	"https://fuss10.elemecdn.com/9/bb/e27858e973f5d7d3904835f46abbdjpeg.jpeg",
-	"https://fuss10.elemecdn.com/d/e6/c4d93a3805b3ce3f323f7974e6f78jpeg.jpeg",
-	"https://fuss10.elemecdn.com/3/28/bbf893f792f03a54408b3b7a7ebf0jpeg.jpeg",
-	"https://fuss10.elemecdn.com/2/11/6535bcfb26e4c79b48ddde44f4b6fjpeg.jpeg"
-];
 
 const handleJoin = (item: any) => {
 	currentRecord.value = item;
@@ -43,7 +34,6 @@ const handleJoin = (item: any) => {
 };
 
 const handleConfirm = async () => {
-	console.log("rule", ruleForm.goodNum);
 	await ruleFormRef.value?.validate();
 	await requestJoin({ groupId: currentRecord.value?.id, goodId: currentRecord.value?.goodId, goodNum: ruleForm.goodNum || 0 });
 	ElMessage({
@@ -102,10 +92,10 @@ watch(dialogVisible, newVal => {
 					</div>
 					<div :class="Classes['item-images']">
 						<el-image
-							v-for="(img, index) of srcList.slice(0, 3)"
+							v-for="(img, index) of item.picUrlList.slice(0, 3)"
 							:key="img"
 							:class="Classes['item-image']"
-							:preview-src-list="srcList"
+							:preview-src-list="item.picUrlList"
 							:initial-index="index"
 							:src="img"
 						/>
@@ -155,6 +145,9 @@ watch(dialogVisible, newVal => {
 			<el-form ref="ruleFormRef" :model="ruleForm" :rules="rules" label-width="120px" class="demo-ruleForm" status-icon>
 				<el-form-item label="商品名称">
 					<span>{{ currentRecord.goodName }}</span>
+				</el-form-item>
+				<el-form-item label="剩余库存">
+					<span>{{ currentRecord.remainingStock }}</span>
 				</el-form-item>
 				<el-form-item label="团购数量" prop="goodNum">
 					<el-input-number v-model="ruleForm.goodNum" :min="1" :max="currentRecord.stock" />

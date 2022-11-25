a140069b60d70b57d4a4ce40b723f12d9cf97a36
dongfang.ding <>
2022-11-25 18:15:11 +0800

    fix 订单状态

--- src/views/order/OrderTab.vue
+++ src/views/order/OrderTab.vue
@@ -4,7 +4,7 @@
 		<el-tabs v-model="activeName" class="demo-tabs">
 			<el-tab-pane label="全部" name=""></el-tab-pane>
 			<el-tab-pane lazy label="待支付" name="WAIT_PAY"></el-tab-pane>
-			<el-tab-pane lazy label="待收货" name="PA"></el-tab-pane>
+			<el-tab-pane lazy label="待收货" name="PAID"></el-tab-pane>
 			<el-tab-pane lazy label="已完成" name="COMPLETE"></el-tab-pane>
 			<el-tab-pane lazy label="已退款" name="REFUND"></el-tab-pane>
 			<el-tab-pane lazy label="已关闭" name="CLOSED"></el-tab-pane>
